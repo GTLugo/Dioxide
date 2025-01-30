4a616c3cf4f4662a15554f434ab74b6f123dbed5
Gabriel Lugo <g.alexander.lugo@gmail.com>
2025-01-29 21:56:42 -0500

    Refactored out common code

--- crates/quark_os_boot/src/bin/vbox-bios.rs
+++ crates/quark_os_boot/src/bin/vbox-bios.rs
@@ -1,112 +1,11 @@
+#![allow(unused_attributes)]
+#![allow(dead_code)]
 #![feature(exit_status_error)]
 
-use std::{
-  env,
-  path::PathBuf,
-  process::{Command, ExitStatusError, Stdio},
-};
+mod vbox;
 
-const NAME: &str = "QuarkOS_BIOS";
+use std::process::ExitStatusError;
 
 fn main() -> Result<(), ExitStatusError> {
-  let img_dir = PathBuf::from(env!("BIOS_IMAGE"));
-  let current_exe = env::current_exe().unwrap();
-  let vdi_dir = current_exe.with_file_name("bios.vdi");
-
-  stop();
-  remove_old_image(&vdi_dir)?;
-  set_new_image(&img_dir, &vdi_dir)?;
-  start()
-}
-
-fn stop() {
-  let mut vm = Command::new("VBoxManage");
-  vm.arg("startvm").arg(NAME).arg("--type").arg("emergencystop");
-  vm.stderr(Stdio::null());
-  let _ = vm.status().unwrap();
-}
-
-fn remove_old_image(vdi_dir: &PathBuf) -> Result<(), ExitStatusError> {
-  let mut vm = Command::new("VBoxManage");
-  vm.arg("storageattach")
-    .arg(NAME)
-    .arg("--storagectl")
-    .arg("IDE")
-    .arg("--port")
-    .arg("0")
-    .arg("--device")
-    .arg("0")
-    .arg("--type")
-    .arg("hdd")
-    .arg("--medium")
-    .arg("none");
-  vm.stderr(Stdio::null());
-  let exit_status = vm.status().unwrap();
-
-  if !exit_status.success() {
-    eprintln!("detach error");
-    // process::exit(exit_status.code().unwrap_or(-1));
-  }
-
-  let mut vm = Command::new("VBoxManage");
-  vm.arg("closemedium").arg("disk").arg(vdi_dir).arg("--delete");
-  vm.stderr(Stdio::null());
-  let exit_status = vm.status().unwrap();
-
-  exit_status.exit_ok()
-
-  // let _ = remove_file(&vdi_dir);
-}
-
-fn set_new_image(img_dir: &PathBuf, vdi_dir: &PathBuf) -> Result<(), ExitStatusError> {
-  let mut vm = Command::new("VBoxManage");
-  vm.arg("convertfromraw")
-    .arg(img_dir)
-    .arg(vdi_dir)
-    .arg("--format")
-    .arg("VDI");
-  vm.stderr(Stdio::null());
-  let exit_status = vm.status().unwrap();
-
-  if !exit_status.success() {
-    eprintln!("convertfromraw error");
-    // process::exit(exit_status.code().unwrap_or(-1));
-  }
-
-  let mut vm = Command::new("VBoxManage");
-  vm.arg("modifymedium").arg(vdi_dir).arg("--resize").arg("2048");
-  vm.stderr(Stdio::null());
-  let exit_status = vm.status().unwrap();
-
-  if !exit_status.success() {
-    eprintln!("modifyhd error");
-    // process::exit(exit_status.code().unwrap_or(-1));
-  }
-
-  let mut vm = Command::new("VBoxManage");
-  vm.arg("storageattach")
-    .arg(NAME)
-    .arg("--storagectl")
-    .arg("IDE")
-    .arg("--port")
-    .arg("0")
-    .arg("--device")
-    .arg("0")
-    .arg("--type")
-    .arg("hdd")
-    .arg("--medium")
-    .arg(vdi_dir);
-  vm.stderr(Stdio::null());
-  let exit_status = vm.status().unwrap();
-
-  exit_status.exit_ok()
-}
-
-fn start() -> Result<(), ExitStatusError> {
-  let mut vm = Command::new("VBoxManage");
-  vm.arg("startvm").arg(NAME);
-  vm.stderr(Stdio::null());
-  let exit_status = vm.status().unwrap();
-
-  exit_status.exit_ok()
+  vbox::VirtualBox::new("QuarkOS_BIOS", env!("BIOS_IMAGE"), vbox::Uefi::Disabled).run()
 }
---
+++ crates/quark_os_boot/src/bin/vbox.rs
@@ -0,0 +1,144 @@
+#![allow(unused_attributes)]
+#![allow(dead_code)]
+#![feature(exit_status_error)]
+
+use std::{
+  fmt::Display,
+  path::PathBuf,
+  process::{Command, ExitStatusError, Stdio},
+};
+
+pub enum Uefi {
+  Enabled,
+  Disabled,
+}
+
+impl Display for Uefi {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    write!(f, "{}", match self {
+      Self::Enabled => "uefi",
+      Self::Disabled => "bios",
+    })
+  }
+}
+
+pub struct VirtualBox {
+  name: String,
+  image: PathBuf,
+  vdi: PathBuf,
+  _uefi: Uefi,
+}
+
+impl VirtualBox {
+  pub fn new(name: impl Into<String>, image: impl Into<PathBuf>, uefi: Uefi) -> Self {
+    let image = image.into();
+    let vdi = image.with_file_name(format!("{uefi}.vdi"));
+    Self {
+      name: name.into(),
+      image,
+      vdi,
+      _uefi: uefi,
+    }
+  }
+
+  pub fn run(&self) -> Result<(), ExitStatusError> {
+    self.stop();
+    self.remove_old_image()?;
+    self.set_new_image()?;
+    self.start()
+  }
+
+  fn stop(&self) {
+    let mut vm = Command::new("VBoxManage");
+    vm.arg("startvm").arg(&self.name).arg("--type").arg("emergencystop");
+    vm.stderr(Stdio::null());
+    let _ = vm.status().unwrap();
+  }
+
+  fn remove_old_image(&self) -> Result<(), ExitStatusError> {
+    let mut vm = Command::new("VBoxManage");
+    vm.arg("storageattach")
+      .arg(&self.name)
+      .arg("--storagectl")
+      .arg("IDE")
+      .arg("--port")
+      .arg("0")
+      .arg("--device")
+      .arg("0")
+      .arg("--type")
+      .arg("hdd")
+      .arg("--medium")
+      .arg("none");
+    vm.stderr(Stdio::null());
+    let exit_status = vm.status().unwrap();
+
+    if !exit_status.success() {
+      eprintln!("detach error");
+      // process::exit(exit_status.code().unwrap_or(-1));
+    }
+
+    let mut vm = Command::new("VBoxManage");
+    vm.arg("closemedium").arg("disk").arg(&self.vdi).arg("--delete");
+    vm.stderr(Stdio::null());
+    let exit_status = vm.status().unwrap();
+
+    exit_status.exit_ok()
+
+    // let _ = remove_file(&vdi_dir);
+  }
+
+  fn set_new_image(&self) -> Result<(), ExitStatusError> {
+    let mut vm = Command::new("VBoxManage");
+    vm.arg("convertfromraw")
+      .arg(&self.image)
+      .arg(&self.vdi)
+      .arg("--format")
+      .arg("VDI");
+    vm.stderr(Stdio::null());
+    let exit_status = vm.status().unwrap();
+
+    if !exit_status.success() {
+      eprintln!("convertfromraw error");
+      // process::exit(exit_status.code().unwrap_or(-1));
+    }
+
+    let mut vm = Command::new("VBoxManage");
+    vm.arg("modifymedium").arg(&self.vdi).arg("--resize").arg("2048");
+    vm.stderr(Stdio::null());
+    let exit_status = vm.status().unwrap();
+
+    if !exit_status.success() {
+      eprintln!("modifyhd error");
+      // process::exit(exit_status.code().unwrap_or(-1));
+    }
+
+    let mut vm = Command::new("VBoxManage");
+    vm.arg("storageattach")
+      .arg(&self.name)
+      .arg("--storagectl")
+      .arg("IDE")
+      .arg("--port")
+      .arg("0")
+      .arg("--device")
+      .arg("0")
+      .arg("--type")
+      .arg("hdd")
+      .arg("--medium")
+      .arg(&self.vdi);
+    vm.stderr(Stdio::null());
+    let exit_status = vm.status().unwrap();
+
+    exit_status.exit_ok()
+  }
+
+  fn start(&self) -> Result<(), ExitStatusError> {
+    let mut vm = Command::new("VBoxManage");
+    vm.arg("startvm").arg(&self.name);
+    vm.stderr(Stdio::null());
+    let exit_status = vm.status().unwrap();
+
+    exit_status.exit_ok()
+  }
+}
+
+fn main() {}
